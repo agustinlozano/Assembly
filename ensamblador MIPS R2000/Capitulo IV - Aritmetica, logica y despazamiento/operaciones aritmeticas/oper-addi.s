7a9ffb03039f4edea330a6e7b049ac914667ac9f
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2021-10-03 20:56:55 -0300

    Add arithmetic operations folder

---
+++ ensamblador MIPS R2000/Capitulo IV - Aritmetica, logica y despazamiento/operaciones aritmeticas/oper-addi.s
@@ -0,0 +1,11 @@
+        .data
+numero: .word 2147483647       # max positivo representable en Ca2(32)
+                               # (en hexa 0x7fff ffff
+
+        .text
+main:   lw    $t0, numero($0)
+        addi  $t1, $t0, 1       # $t1 <- $t0 + 1
+                                # $t1 = 0x7fffffff
+
+# No realizo la suma correctamente porque la cifra a representar
+# estaba fuera del rango de los positivos representables
---
+++ ensamblador MIPS R2000/Capitulo IV - Aritmetica, logica y despazamiento/operaciones aritmeticas/oper-addiu.s
@@ -0,0 +1,8 @@
+.data
+numero: .word 2147483647       # max positivo representable en Ca2(32)
+                               # (en hexa 0x7fff ffff
+
+.text
+main:   lw    $t0, numero($0)
+        addiu $t1, $t0, 1       # $t1 <- $t0 + 1
+                                # $t1 = 80000000
