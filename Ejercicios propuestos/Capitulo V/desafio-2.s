ff85e18a68526f12994805c85ba229dbcbc6bf8c
Agustin Lozano Blua <agustinlozanoblua@gmail.com>
2021-10-18 14:28:44 -0300

    Add desafio-2 chapter V resolution

--- Ejercicios propuestos/Capitulo V/desafio-2.s
+++ Ejercicios propuestos/Capitulo V/desafio-2.s
@@ -1,25 +1,21 @@
                 .data
-vector:         .word   2, -4, -6
+vector:         .word   2, -4, 5
         
-                .data   10010010
 res:            .space  3
 
                 .text
-main:           lw      $t7, vector($0)
-                lw      $t8, vector+4($0)
-                lw      $t9, vector+8($0)
+main:           lw      $t5, vector($0)
+                lw      $t6, vector+4($0)
+                lw      $t7, vector+8($0)
                 
                 ori		$t0, $0, 0
                 ori		$t1, $0, 0
                 ori		$t2, $0, 0
 
-                sge     $t0, $t7, $0
-                sge     $t1, $t8, $0
-                sge     $t2, $t9, $0
+                sge     $t0, $t5, $0
+                sge     $t1, $t6, $0
+                sge     $t2, $t7, $0
 
-                and     $t0, $t0, $t1
-                and     $t0, $t0, $t3
-                
-                bgez    $t0, valorfinal
-
-valorfinal:     sb      $t0, res($0)
+                sb      $t0, res($0)
+                sb      $t1, res+1($0)
+                sb      $t2, res+2($0)
